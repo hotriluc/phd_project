14769a511b29f6f2b20bcc4255c75d7d6e0b968e
hotriluc <hotriluc97@gmail.com>
2021-11-14 19:40:50 +0200

    ofdm

---
+++ testing/noise_immunity/ofdm.py
@@ -0,0 +1,14 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import math
+import scipy.interpolate
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
+import mpl_toolkits.mplot3d.art3d as art3d
+import gray_codes as gray
+
+
+
+def n_likes(n, array):
+    return [n]*len(array)
